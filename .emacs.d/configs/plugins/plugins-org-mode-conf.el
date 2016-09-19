5a75e5ea3c2634fd1dd786ca18b4266470f2cb23
Egor Churaev <egor.churaev@intel.com>
2016-09-19 13:19:22 +0300

    Added new todo keywords to org-mode

--- .emacs.d/configs/plugins/plugins-org-mode-conf.el
+++ .emacs.d/configs/plugins/plugins-org-mode-conf.el
@@ -5,4 +5,15 @@
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
 
+(setq org-todo-keywords
+       '((sequence "TODO" "WIP" "PENDING" "|" "DONE")))
+(setq org-todo-keyword-faces
+'(("TODO". (:foreground "red" :weight bold))
+("WIP". (:foreground "#57b0ff" :weight bold))
+("PENDING". (:foreground "orange" :weight bold))
+("DONE". (:foreground "green" :weight bold)))
+)
+
+(global-font-lock-mode 1)
+
 (provide 'plugins-org-mode-conf)
