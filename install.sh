369ab3a1dc81d99a4368ca5d5967669a6f61480a
Egor Churaev <egor.churaev@intel.com>
2017-07-03 17:16:41 +0300

    Fixed install script

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 install_dir=$HOME
+FILES=$(dirname "$0")/*
+HFILES=$(dirname "$0")/.*
 
-for f in *; do
+for f in $FILES; do
+    cp -rf $f $install_dir
+done
+
+for f in $HFILES; do
     cp -rf $f $install_dir
 done
