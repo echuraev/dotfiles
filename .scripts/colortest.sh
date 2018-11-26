0274c0abfdd8e8cbc49c475ace270f1f1b033dd5
Egor Churaev <egor.churaev@retailnext.net>
2018-11-26 18:06:01 +0300

    Added colortest script

---
+++ .scripts/colortest.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Take it from here: https://github.com/huyvohcmc/dotfiles/blob/95f4494d55f7ecfecec333003e9fc296d18aa07c/macos/colortest
+
+# Usage
+# colortest
+
+DOTS='•••'
+printf "\n                 40m     41m     42m     43m     44m     45m     46m     47m\n";
+for FGs in '    m' '   1m' '  30m' '1;30m' '  31m' '1;31m' '  32m' \
+           '1;32m' '  33m' '1;33m' '  34m' '1;34m' '  35m' '1;35m' \
+           '  36m' '1;36m' '  37m' '1;37m'; do
+  FG=${FGs// /}
+  printf " $FGs \033[$FG  $DOTS  "
+  for BG in 40m 41m 42m 43m 44m 45m 46m 47m; do
+    printf "$EINS \033[$FG\033[$BG  $DOTS  \033[0m";
+  done
+  printf "\n"
+done
+printf "\n"
+unset -v DOTS
+
