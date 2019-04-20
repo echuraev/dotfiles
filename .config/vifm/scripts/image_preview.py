eda7ee9f0d98bb6372f68a999a25ff443dba799d
Egor Churaev <egor.churaev@retailnext.net>
2019-04-21 00:12:06 +0300

    vifm: Initial version of preview image script

---
+++ .config/vifm/scripts/image_preview.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+
+DEFAULT_LIMIT = 1000000       # 1Mb
+DEFAULT_TEXT_LIMIT = 3000000  # 3Mb
+
+def preview_image(args):
+    cmd = ""
+    img_size = os.path.getsize(args.image)
+    if img_size <= args.max_size:
+        preview_bin = "/usr/local/bin/term-img"
+        if args.width != None and args.height != None:
+            cmd = '{0} --width {1} --height {2} "{3}" > /dev/tty'.format(preview_bin, args.width, args.height, args.image)
+        else:
+            cmd = '{0} "{1}" > /dev/tty'.format(preview_bin, args.image)
+    elif img_size <= args.max_text_size:
+        cmd = 'cd "{0}" && convert -identify {1} -verbose /dev/null'.format(os.path.dirname(args.image), os.path.basename(args.image))
+
+    if len(cmd) > 0:
+        os.system(cmd)
+    else:
+        print("Image is too large for preview")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Script for preview images")
+    parser.add_argument('image', type=str, help="Path to image file")
+    parser.add_argument('--width', type=str, help="Width of preview image")
+    parser.add_argument('--height', type=str, help="Height of preview image")
+    parser.add_argument('--max_size', type=int, default=DEFAULT_LIMIT,
+            help="Won't show preview for images more when this size (Default: {0} bytes)".format(DEFAULT_LIMIT))
+    parser.add_argument('--max_text_size', type=int, default=DEFAULT_TEXT_LIMIT,
+            help="Won't show text preview for images more when this size (Default: {0} bytes)".format(DEFAULT_TEXT_LIMIT))
+
+    args = parser.parse_args()
+    preview_image(args)
+
