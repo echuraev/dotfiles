6e6a3e0413ecb6295d1acd77075ae2a1c6c74edb
Egor Churaev <egor.churaev@retailnext.net>
2019-06-20 13:24:22 +0300

    Fixed install script

--- install.sh
+++ install.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+initial_dir=$(pwd)
+#script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" >/dev/null 2>&1 && pwd  )"
+script_dir=$(dirname "$0")
+cd $script_dir
+
+git submodule update --init --recursive
+
 install_dir=$HOME
-FILES=$(dirname "$0")/*
-HFILES=$(dirname "$0")/.*
+FILES=`ls -a -1`
 
 for f in $FILES; do
     cp -rf $f $install_dir
 done
 
-for f in $HFILES; do
-    cp -rf $f $install_dir
-done
-
-cd $install_dir
-git submodule update --init --recursive
+cd $initial_dir
